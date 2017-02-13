ed87b3b3dfa8eb699773f8e82f64d6375ae0f81e
Jason Pierna <siilver777@me.com>
2017-02-13 16:18:39 +0100

    Debug page with db content

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ var sqlite3 = require('sqlite3').verbose();
 
 var app = express();
 
+var db = new sqlite3.Database('messages.db');
 var port = process.env.PORT || 8080;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/messages', function(req, res) {
-    var db = new sqlite3.Database('messages.db');
+app.get('/', function(req, res) {
+    var messages = [];
 
+	db.serialize(function() {
+		db.each("SELECT messages.rowid AS id, messages.number, messages.message, messages.lat, messages.lng FROM messages ORDER BY id", function(err, row) {
+			if (err) {
+				console.error(err);
+			}
+			else {
+				var message = {
+					'id': row.id,
+					'number': row.number,
+					'message': row.description,
+					'lat': row.lat,
+					'lng': row.lng
+				};
+				messages.push(message);
+			}
+		}, function() {
+			res.setHeader('Content-Type', 'application/json');
+			res.send(JSON.stringify(messages));
+		});
+	});
+	db.close();
+});
+
+app.post('/messages', function(req, res) {
     var message = {
         'number': req.body.number,
         'message': req.body.message,
